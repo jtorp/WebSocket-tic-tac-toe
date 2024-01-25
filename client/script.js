a256878609b6fcaadf18ccfe12f157ec1841f12e
Christian Torp <torp@Christians-Air.home>
2024-01-25 16:06:57 +0100

    updated client scripts

--- client/script.js
+++ client/script.js
@@ -1,10 +1,40 @@
 let ws = new WebSocket('ws://localhost:8081');
-const wsMsg =document.getElementById('ws')
-ws.onmessage =  (msg) => {
-    const resp = JSON.parse(msg.data);
-    console.log(resp)
-    if(resp.message.includes('Welcome to the Tic Tac Toe Game')) {
-        document.getElementById('ws').innerHTML = resp.message
+let symbol = null;
+let turn = null;
+let isGameActive = false;
+let serverMsg='';
+let cellsGrid = [
+    '', '', '',
+    '', '', '',
+    '', '', ''
+]
+
+const wsMsg = document.getElementById('wsMessage');
+const cells = document.querySelectorAll('.cell');
+
+cells.forEach((cell, index) => {
+    cell.addEventListener('click', (event) => {
+      makeMove(event.target, index);   
+})
+})
+
+function makeMove(cell, index) {
+}
+
+ws.onmessage = (message) => {
+    const response = JSON.parse(message.data);
+    if (response.method === 'join') {
+        symbol = response.symbol;
+        turn = response.turn;
+        isGameActive = symbol === turn;
+        updateMessage();
     }
-ws.send(JSON.stringify({message: 'Hi, from client'}))
+}
+
+function updateMessage() {
+    if (symbol === turn) {
+        wsMsg.textContent = 'Your Move';
+    } else {
+        wsMsg.textContent = `Awaiting ${turn}'s turn`;
+    }
 }
